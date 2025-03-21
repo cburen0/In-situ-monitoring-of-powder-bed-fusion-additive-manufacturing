195c178bb68882c34e3317e7b99730fcfb6231ed
21Whetham <168486968+21Whetham@users.noreply.github.com>
2025-03-21 15:32:35 -0400

    New Scanner CAD

--- system_development/Orichalcum/v0.2/Software_design/data-processing-code/Trigger and LDV Waves_v2.py
+++ system_development/Orichalcum/v0.2/Software_design/data-processing-code/Trigger and LDV Waves_v2.py
@@ -35,28 +35,58 @@ fs = 2500000000  # Sampling frequency (Hz)
 
 # file names
 trigger_filename = r"C:\Users\mwhetham\Desktop\Trigger (pulser) and Wave Signal (LDV) read from PXI\trigger-000-000-"
-ldv_1 = r"C:\Users\mwhetham\Desktop\Trigger (pulser) and Wave Signal (LDV) read from PXI\ldv-000-000-"
+ldv_1 = r"C:\Users\mwhetham\Desktop\Trigger (pulser) and Wave Signal (LDV) read from PXI\ldv_1-000-000-"
+ldv_3 = r"C:\Users\mwhetham\Desktop\Trigger (pulser) and Wave Signal (LDV) read from PXI\ldv_3-000-000-"
+ldv_5 = r"C:\Users\mwhetham\Desktop\Trigger (pulser) and Wave Signal (LDV) read from PXI\ldv_5-000-000-"
+ldv_7 = r"C:\Users\mwhetham\Desktop\Trigger (pulser) and Wave Signal (LDV) read from PXI\ldv_7-000-000-"
 
 # Load data
-time_ldv, signal_ldv = load_waveform(ldv_1, fs)
+time_ldv1, signal_ldv1 = load_waveform(ldv_1, fs)
+time_ldv3, signal_ldv3 = load_waveform(ldv_3, fs)
+time_ldv5, signal_ldv5 = load_waveform(ldv_5, fs)
+time_ldv7, signal_ldv7 = load_waveform(ldv_7, fs)
+
 time_trigger, signal_trigger = load_waveform(trigger_filename, fs)
 
+rows = 5
+
 #make figure
-plt.figure(figsize=(10, 8))
+plt.figure(figsize=(10, 16))
 plt.rcParams['font.family'] = 'Times New Roman'
 plt.rcParams['font.size'] = 15
 
 # Receiver Output
-plt.subplot(2, 1, 1)
+plt.subplot(rows, 1, 1)
 plt.plot(time_trigger * 1e6, signal_trigger, color='m')
 plt.xlabel("Time (μs)\n(a)")
 plt.ylabel("Amplitude")
 plt.grid()
 
-# LDV velocity
-plt.subplot(2, 1, 2)
-plt.plot(time_ldv * 1e6, signal_ldv, color='c')
+# LDV 1
+plt.subplot(rows, 1, 2)
+plt.plot(time_ldv1 * 1e6, signal_ldv1, color='c')
 plt.xlabel("Time (μs)\n(b)")
+plt.ylabel("Amplitude")
+plt.grid()
+
+# LDV 3
+plt.subplot(rows, 1, 3)
+plt.plot(time_ldv3 * 1e6, signal_ldv3, color='c')
+plt.xlabel("Time (μs)\n(c)")
+plt.ylabel("Amplitude")
+plt.grid()
+
+# LDV 5
+plt.subplot(rows, 1, 4)
+plt.plot(time_ldv5 * 1e6, signal_ldv5, color='c')
+plt.xlabel("Time (μs)\n(d)")
+plt.ylabel("Amplitude")
+plt.grid()
+
+# LDV 7
+plt.subplot(rows, 1, 5)
+plt.plot(time_ldv7 * 1e6, signal_ldv7, color='c')
+plt.xlabel("Time (μs)\n(e)")
 plt.ylabel("Amplitude")
 plt.grid()
 
